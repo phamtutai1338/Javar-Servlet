81c77b24dfde9399fb1bbaaf1bf782348ec69f7d
phạm tú tài <93572750+phamtutai1338@users.noreply.github.com>
2023-07-31 14:11:09 +0700

    up

--- API/AssignmentAPI/asmcustomerapi/src/main/java/com/example/asmcustomerapi/config/SecurityConfig.java
+++ API/AssignmentAPI/asmcustomerapi/src/main/java/com/example/asmcustomerapi/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/api/customer").hasAnyRole("ADMIN","MANAGER","EMPLOYEE")
+                .antMatchers("/api/customer").hasAnyRole("ADMIN","MANAGER","USER")
                 .antMatchers("/api/customer/{id}").hasRole("ADMIN")
                 .and()
                 .httpBasic()
